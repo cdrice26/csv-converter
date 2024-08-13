ceb669404a47c63321fe5f392280d35728944251
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2024-08-13 11:34:53 -0400

    Added function to get positional arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use std::{error::Error, process};
+use std::{env, error::Error, process};
 
 fn main() {
-    // Create a CSV parser that takes file path from the first command-line argument 
-    let raw_path = std::env::args().nth(1).expect("Please provide an input file path");
+    // Create a CSV parser that takes file path from the first command-line argument
+    let raw_path = match get_arg(1) {
+        Ok(path) => path,
+        Err(e) => {
+            println!("Error getting argument: {}", e);
+            process::exit(1);
+        }
+    };
     let absolute_path = match get_file_path(&raw_path) {
         Ok(absolute_path) => absolute_path,
         Err(e) => {
@@ -18,6 +24,7 @@ fn main() {
     }
 }
 
+/// Returns the absolute path to the given file.
 fn get_file_path(raw_path: &str) -> Result<String, Box<dyn Error>> {
     let full_path = std::path::Path::new(&raw_path);
     if !full_path.exists() {
@@ -27,6 +34,21 @@ fn get_file_path(raw_path: &str) -> Result<String, Box<dyn Error>> {
     Ok(absolute_path.display().to_string())
 }
 
+/// Returns the nth positional argument sent to this process. If there are not
+/// enough positional arguments, then this returns an error.
+fn get_arg(idx: usize) -> Result<String, Box<dyn Error>> {
+    match env::args_os().nth(idx) {
+        None => Err(From::from(format!(
+            "expected {} arguments, but got none",
+            idx
+        ))),
+        Some(arg) => arg
+            .to_str()
+            .map(|s| s.to_string())
+            .ok_or_else(|| From::from("argument is not valid UTF-8")),
+    }
+}
+
 fn run(absolute_path: &str) -> Result<(), Box<dyn Error>> {
     let mut parser = csv::Reader::from_path(absolute_path).unwrap();
 
@@ -34,7 +56,7 @@ fn run(absolute_path: &str) -> Result<(), Box<dyn Error>> {
     for record in parser.records() {
         match record {
             Ok(record) => println!("{:?}", record),
-            Err(e) => return Err(From::from(e))
+            Err(e) => return Err(From::from(e)),
         }
     }
     Ok(())
