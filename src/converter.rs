0cfeff6dcd1f225c0eb5d7f7dd41e8343972941f
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2024-08-13 18:18:21 -0400

    Rudimentary conversion for length

--- src/converter.rs
+++ src/converter.rs
@@ -2,12 +2,37 @@ use std::error::Error;
 
 use crate::normalizer;
 
-fn mm_to_cm(mm: f64) -> f64 {
-    mm / 10.0
-}
+pub mod length;
 
 pub fn convert(val: f64, from: &str, to: &str) -> Result<f64, Box<dyn Error>> {
     let normalized_from = normalizer::normalize(from)?;
+    let converted_to_std = match normalized_from {
+        "km" => length::km_to_mm(val),
+        "cm" => length::cm_to_mm(val),
+        "um" => length::um_to_mm(val),
+        "nm" => length::nm_to_mm(val),
+        "mi" => length::mi_to_mm(val),
+        "yd" => length::yd_to_mm(val),
+        "in" => length::in_to_mm(val),
+        "ft" => length::ft_to_mm(val),
+        "m" => length::m_to_mm(val),
+        "mm" => val,
+        _ => return Err("from unit not recognized".into()),
+    };
+
     let normalized_to = normalizer::normalize(to)?;
-    Ok(val / 10.0)
+    let converted_to_desired = match normalized_to {
+        "km" => length::mm_to_km(converted_to_std),
+        "cm" => length::mm_to_cm(converted_to_std),
+        "um" => length::mm_to_um(converted_to_std),
+        "nm" => length::mm_to_nm(converted_to_std),
+        "mi" => length::mm_to_mi(converted_to_std),
+        "yd" => length::mm_to_yd(converted_to_std),
+        "in" => length::mm_to_in(converted_to_std),
+        "ft" => length::mm_to_ft(converted_to_std),
+        "m" => length::mm_to_m(converted_to_std),
+        "mm" => converted_to_std,
+        _ => return Err("to unit not recognized".into()),
+    };
+    Ok(converted_to_desired)
 }
---
+++ src/converter/length.rs
@@ -0,0 +1,71 @@
+pub fn m_to_mm(m: f64) -> f64 {
+    m * 1000.0
+}
+
+pub fn km_to_mm(km: f64) -> f64 {
+    km * 1000000.0
+}
+
+pub fn cm_to_mm(cm: f64) -> f64 {
+    cm * 10.0
+}
+
+pub fn um_to_mm(um: f64) -> f64 {
+    um * 0.001
+}
+
+pub fn nm_to_mm(nm: f64) -> f64 {
+    nm * 0.000001
+}
+
+pub fn mi_to_mm(mi: f64) -> f64 {
+    mi * 1609344.0
+}
+
+pub fn yd_to_mm(yd: f64) -> f64 {
+    yd * 914.4
+}
+
+pub fn ft_to_mm(ft: f64) -> f64 {
+    ft * 304.8
+}
+
+pub fn in_to_mm(inch: f64) -> f64 {
+    inch * 25.4
+}
+
+pub fn mm_to_m(m: f64) -> f64 {
+    m / 1000.0
+}
+
+pub fn mm_to_km(km: f64) -> f64 {
+    km / 1000000.0
+}
+
+pub fn mm_to_cm(cm: f64) -> f64 {
+    cm / 10.0
+}
+
+pub fn mm_to_um(um: f64) -> f64 {
+    um / 0.001
+}
+
+pub fn mm_to_nm(nm: f64) -> f64 {
+    nm / 0.000001
+}
+
+pub fn mm_to_mi(mi: f64) -> f64 {
+    mi / 1609344.0
+}
+
+pub fn mm_to_yd(yd: f64) -> f64 {
+    yd / 914.4
+}
+
+pub fn mm_to_ft(ft: f64) -> f64 {
+    ft / 304.8
+}
+
+pub fn mm_to_in(inch: f64) -> f64 {
+    inch / 25.4
+}
